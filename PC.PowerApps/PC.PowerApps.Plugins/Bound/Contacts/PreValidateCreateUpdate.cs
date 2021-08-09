be7396db0be9467d68a9b5a6d137276464842bcb
Mihails Simvulidi <mihails.simvulidi@gmail.com>
2021-08-10 02:41:51 +0300

    Contact validation improvements

--- PC.PowerApps/PC.PowerApps.Plugins/Bound/Contacts/PreValidateCreateUpdate.cs
+++ PC.PowerApps/PC.PowerApps.Plugins/Bound/Contacts/PreValidateCreateUpdate.cs
@@ -11,7 +11,7 @@ namespace PC.PowerApps.Plugins.Bound.Contacts
         {
             PreCreateUpdatePluginContext<Contact> context = new(serviceProvider, OrganizationServiceUser.System, OrganizationServiceUser.User);
             context.EnsureAttributesNotModified(c => new { c.pc_PaidParticipationFee, c.pc_ParticipationLevel, c.pc_RequiredParticipationFee, c.pc_SentDebtReminderOn, c.pc_SentSupporterWelcomeEmailOn, c.StatusCode });
-            context.EnsureModifiedAttributesNotEmpty(c => new { c.Description, c.EMailAddress1, c.FirstName, c.LastName, c.pc_PersonalIdentityNumber });
+            context.EnsureModifiedAttributesNotEmpty(c => new { c.Description, c.EMailAddress1, c.FirstName, c.LastName, c.pc_IsInvitedToSlack, c.pc_PersonalIdentityNumber, c.pc_WishesToBeActive });
         }
     }
 }
