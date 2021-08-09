a3492df9b3aedcbff1f4e85472ef150ea42bf665
Mihails Simvulidi <mihails.simvulidi@gmail.com>
2021-08-10 02:56:19 +0300

    Email validation implementation

---
+++ PC.PowerApps/PC.PowerApps.Plugins/Bound/Emails/PreValidateCreateUpdate.cs
@@ -0,0 +1,16 @@
+﻿using PC.PowerApps.Common.Entities.Dataverse;
+using PC.PowerApps.Plugins.Contexts;
+using PC.PowerApps.Plugins.Enumerations;
+using System;
+
+namespace PC.PowerApps.Plugins.Bound.Emails
+{
+    public class PreValidateCreateUpdate : PluginBase
+    {
+        protected override void ExecuteInternal(IServiceProvider serviceProvider)
+        {
+            PreCreateUpdatePluginContext<Email> context = new(serviceProvider, OrganizationServiceUser.System, OrganizationServiceUser.User);
+            context.EnsureAttributesNotModified(e => e.pc_Category);
+        }
+    }
+}
