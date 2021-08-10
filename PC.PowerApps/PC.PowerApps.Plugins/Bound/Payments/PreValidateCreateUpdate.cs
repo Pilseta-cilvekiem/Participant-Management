cb7cdf9a92d35eaa5f21b49e9050a45833522e51
Mihails Simvulidi <mihails.simvulidi@gmail.com>
2021-08-10 15:34:48 +0300

    Payment validation implementation

---
+++ PC.PowerApps/PC.PowerApps.Plugins/Bound/Payments/PreValidateCreateUpdate.cs
@@ -0,0 +1,29 @@
+﻿using PC.PowerApps.Common.Entities.Dataverse;
+using PC.PowerApps.Plugins.Contexts;
+using PC.PowerApps.Plugins.Enumerations;
+using System;
+
+namespace PC.PowerApps.Plugins.Bound.Payments
+{
+    public class PreValidateCreateUpdate : PluginBase
+    {
+        protected override void ExecuteInternal(IServiceProvider serviceProvider)
+        {
+            PreCreateUpdatePluginContext<pc_Payment> context = new(serviceProvider, OrganizationServiceUser.System, OrganizationServiceUser.User);
+            context.EnsureAttributesNotModified(p => p.pc_Date);
+            pc_Payment payment = context.PostImage;
+
+            if (payment.StatusCode != pc_Payment_StatusCode.Active)
+            {
+                context.EnsureAttributesNotModified(p => p.StatusCode);
+            }
+
+            context.EnsureModifiedAttributesNotEmpty(p => new { p.pc_Contact, p.pc_Transaction });
+
+            if (!context.GetIsAnyAttributeModified(p => p.pc_Transaction))
+            {
+                context.EnsureModifiedAttributesNotEmpty(p => p.pc_Amount);
+            }
+        }
+    }
+}
