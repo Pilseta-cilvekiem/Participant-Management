cf30ab74a1d920ea9f75df403a0955618986d8ee
Mihails Simvulidi <mihails.simvulidi@gmail.com>
2021-08-12 00:31:33 +0300

    Settings validation implementation

---
+++ PC.PowerApps/PC.PowerApps.Common/Repositories/SettingsRepository.cs
@@ -0,0 +1,17 @@
+﻿using PC.PowerApps.Common.Entities.Dataverse;
+using System;
+using System.Linq;
+
+namespace PC.PowerApps.Common.Repositories
+{
+    public static class SettingsRepository
+    {
+        public static pc_Settings GetAnotherActiveSettings(Context context, Guid settingsId)
+        {
+            pc_Settings anotherActiveSettings = context.ServiceContext.pc_SettingsSet
+                .Where(s => s.StatusCode == pc_Settings_StatusCode.Active && s.Id != settingsId)
+                .FirstOrDefault();
+            return anotherActiveSettings;
+        }
+    }
+}
