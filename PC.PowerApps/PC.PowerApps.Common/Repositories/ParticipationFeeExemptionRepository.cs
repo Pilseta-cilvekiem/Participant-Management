e6f1b2060864935910939f56f08c0e37677be264
Mihails Simvulidi <mihails.simvulidi@gmail.com>
2021-08-11 15:56:40 +0300

    Participation fee exemption validation implementation

---
+++ PC.PowerApps/PC.PowerApps.Common/Repositories/ParticipationFeeExemptionRepository.cs
@@ -0,0 +1,23 @@
+﻿using PC.PowerApps.Common.Entities.Dataverse;
+using System.Linq;
+
+namespace PC.PowerApps.Common.Repositories
+{
+    public static class ParticipationFeeExemptionRepository
+    {
+        public static pc_ParticipationFeeExemption GetParticipationFeeExemptionWithinSamePeriod(Context context, pc_ParticipationFeeExemption participationFeeExemption)
+        {
+            IQueryable<pc_ParticipationFeeExemption> otherParticipationFeeExemptionQuery = context.ServiceContext.pc_ParticipationFeeExemptionSet
+                .Where(pfe => pfe.pc_Contact.Id == participationFeeExemption.pc_Contact.Id && pfe.Id != participationFeeExemption.Id && (pfe.pc_Till == null || pfe.pc_Till >= participationFeeExemption.pc_From));
+
+            if (participationFeeExemption.pc_Till is not null)
+            {
+                otherParticipationFeeExemptionQuery = otherParticipationFeeExemptionQuery
+                    .Where(p => p.pc_From <= participationFeeExemption.pc_Till);
+            }
+
+            pc_ParticipationFeeExemption otherParticipationFeeExemption = otherParticipationFeeExemptionQuery.FirstOrDefault();
+            return otherParticipationFeeExemption;
+        }
+    }
+}
---
+++ PC.PowerApps/PC.PowerApps.Plugins/Bound/ParticipationFeeExemption/PreValidateCreateUpdate.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xrm.Sdk;
+using PC.PowerApps.Common.Entities.Dataverse;
+using PC.PowerApps.Common.Repositories;
+using PC.PowerApps.Plugins.Contexts;
+using PC.PowerApps.Plugins.Enumerations;
+using System;
+
+namespace PC.PowerApps.Plugins.Bound.ParticipationFeeExemption
+{
+    public class PreValidateCreateUpdate : PluginBase
+    {
+        protected override void ExecuteInternal(IServiceProvider serviceProvider)
+        {
+            PreCreateUpdatePluginContext<pc_ParticipationFeeExemption> context = new(serviceProvider, User.System, User.User);
+
+            if (!context.GetIsValidationEnabled())
+            {
+                return;
+            }
+
+            pc_ParticipationFeeExemption participationFeeExemption = context.PostImage;
+
+            if (participationFeeExemption.StatusCode != pc_ParticipationFeeExemption_StatusCode.Active)
+            {
+                context.EnsureAttributesNotModified(pfe => pfe.StatusCode);
+            }
+
+            context.EnsureCreatedOrUpdatedAttributesNotEmpty(pfe => new { pfe.pc_Contact, pfe.pc_Description, pfe.pc_From });
+
+            if (context.GetIsAnyAttributeModified(pfe => new { pfe.pc_From, pfe.pc_Till }))
+            {
+                if (participationFeeExemption.pc_From > participationFeeExemption.pc_Till)
+                {
+                    throw new InvalidPluginExecutionException($"Participation Fee Exemption From must be less than or equal to Till.");
+                }
+
+                pc_ParticipationFeeExemption otherParticipationFeeExemption = ParticipationFeeExemptionRepository.GetParticipationFeeExemptionWithinSamePeriod(context, participationFeeExemption);
+
+                if (otherParticipationFeeExemption is not null)
+                {
+                    throw new InvalidPluginExecutionException($"There is another Participation Fee Exemption for this Contact within the same period.");
+                }
+            }
+        }
+    }
+}
