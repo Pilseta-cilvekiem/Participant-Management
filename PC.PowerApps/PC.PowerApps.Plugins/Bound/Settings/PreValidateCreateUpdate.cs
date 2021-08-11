394622e8c6c0faaa68c85037d0a69bd4de06b747
Mihails Simvulidi <mihails.simvulidi@gmail.com>
2021-08-12 00:31:33 +0300

    Settings validation implementation

---
+++ PC.PowerApps/PC.PowerApps.Plugins/Bound/Settings/PreValidateCreateUpdate.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xrm.Sdk;
+using PC.PowerApps.Common.Entities.Dataverse;
+using PC.PowerApps.Common.Repositories;
+using PC.PowerApps.Plugins.Contexts;
+using PC.PowerApps.Plugins.Enumerations;
+using System;
+
+namespace PC.PowerApps.Plugins.Bound.Settings
+{
+    public class PreValidateCreateUpdate : PluginBase
+    {
+        protected override void ExecuteInternal(IServiceProvider serviceProvider)
+        {
+            PreCreateUpdatePluginContext<pc_Settings> context = new(serviceProvider, User.System, User.User);
+
+            if (context.IsValidationDisabled)
+            {
+                return;
+            }
+
+            context.EnsureCreatedOrUpdatedAttributesNotEmpty(s => new { s.pc_AutomaticallyProcessTransactions, s.pc_Name });
+            pc_Settings settings = context.PostImage;
+            pc_Settings anotherActiveSettings = SettingsRepository.GetAnotherActiveSettings(context, settings.Id);
+
+            if (anotherActiveSettings is not null)
+            {
+                throw new InvalidPluginExecutionException("There are already another active Settings.");
+            }
+        }
+    }
+}
