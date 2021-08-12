42529d7376e43c851969dc5b78076e91c9c32078
Mihails Simvulidi <mihails.simvulidi@gmail.com>
2021-08-12 18:38:34 +0300

    Participation Fee Exemption validation improvements

---
+++ PC.PowerApps/PC.PowerApps.Common/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PC.PowerApps.Common.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static bool IsLastDayOfMonth(this DateTime dateTime)
+        {
+            int daysInMonth = DateTime.DaysInMonth(dateTime.Year, dateTime.Month);
+            return dateTime.Day == daysInMonth;
+        }
+    }
+}
