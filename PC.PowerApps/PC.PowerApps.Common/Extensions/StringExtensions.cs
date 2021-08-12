0da615b2a0772fd2d5531a3aba7c8b9b9ce89e3a
Mihails Simvulidi <mihails.simvulidi@gmail.com>
2021-08-12 15:51:14 +0300

    Naming creation refactoring

--- PC.PowerApps/PC.PowerApps.Common/Extensions/StringExtensions.cs
+++ PC.PowerApps/PC.PowerApps.Common/Extensions/StringExtensions.cs
@@ -4,7 +4,8 @@
     {
         public static string TakeFirst(this string @string, int maxLength)
         {
-            return @string == null || @string.Length <= maxLength ? @string : @string.Substring(0, maxLength);
+            const string Dots = "...";
+            return @string is null || @string.Length <= maxLength ? @string : $"{@string.Substring(0, maxLength - Dots.Length).TrimEnd()}{Dots}";
         }
     }
 }
