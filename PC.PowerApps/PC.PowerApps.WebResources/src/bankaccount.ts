87d240a19535148372269d7678f960da9d2e4cde
Mihails Simvulidi <mihails.simvulidi@gmail.com>
2021-12-26 13:53:57 +0200

    Bank account import transaction refresh script

---
+++ PC.PowerApps/PC.PowerApps.WebResources/src/bankaccount.ts
@@ -0,0 +1,31 @@
+﻿import { sleep } from "./lib/common";
+
+let form: Form.pc_bankaccount.Main.Information;
+let isFormLoaded = false;
+
+export function onLoad(executionContext: Xrm.ExecutionContext<any, any>) {
+    form = <Form.pc_bankaccount.Main.Information>executionContext.getFormContext();
+
+    if (isFormLoaded) {
+        return;
+    }
+
+    form.getAttribute("pc_transactionimportfile").addOnChange(onTransactionImportFileChange);
+
+    isFormLoaded = true;
+}
+
+async function onTransactionImportFileChange() {
+    if (!form.getAttribute("pc_transactionimportfile").getValue()) {
+        return;
+    }
+
+    while (true) {
+        await sleep(5000);
+        await form.data.refresh();
+        const transactionImportStatus = form.getAttribute("pc_transactionimportstatus").getValue();
+        if (transactionImportStatus != pc_transactionimportstatus.Pending && transactionImportStatus != pc_transactionimportstatus.InProgress) {
+            return;
+        }
+    }
+}
