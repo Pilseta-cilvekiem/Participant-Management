8db53d91718fd857028a67dc382628f3e5af9fd7
Mihails Simvulidi <mihails.simvulidi@gmail.com>
2021-12-26 13:53:57 +0200

    Bank account import transaction refresh script

---
+++ PC.PowerApps/PC.PowerApps.WebResources/typings/XRM/_internal/Enum/pc_bankaccount_statuscode.d.ts
@@ -0,0 +1,4 @@
+declare const enum pc_bankaccount_statuscode {
+  Active = 1,
+  Inactive = 2,
+}
--- PC.PowerApps/PC.PowerApps.WebResources/typings/XRM/_internal/web-entities.d.ts
+++ PC.PowerApps/PC.PowerApps.WebResources/typings/XRM/_internal/web-entities.d.ts
@@ -9,6 +9,29 @@ interface WebEntity {
 interface WebEntity_Fixed {
   "@odata.etag": string;
 }
+interface pc_BankAccount_Base extends WebEntity {
+}
+interface pc_BankAccount_Fixed extends WebEntity_Fixed {
+  pc_bankaccountid: string;
+}
+interface pc_BankAccount extends pc_BankAccount_Base, pc_BankAccount_Relationships {
+}
+interface pc_BankAccount_Relationships {
+}
+interface pc_BankAccount_Result extends pc_BankAccount_Base, pc_BankAccount_Relationships {
+}
+interface pc_BankAccount_FormattedResult {
+}
+interface pc_BankAccount_Select {
+}
+interface pc_BankAccount_Expand {
+}
+interface pc_BankAccount_Filter {
+}
+interface pc_BankAccount_Create extends pc_BankAccount {
+}
+interface pc_BankAccount_Update extends pc_BankAccount {
+}
 interface WebResource_Base extends WebEntity {
 }
 interface WebResource_Fixed extends WebEntity_Fixed {
