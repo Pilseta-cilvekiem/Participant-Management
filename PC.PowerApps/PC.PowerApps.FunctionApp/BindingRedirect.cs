3993f501bbacaa8629967fc5e725bd3038f58cc3
Mihails Simvulidi <mihails.simvulidi@gmail.com>
2022-02-10 13:34:01 +0200

    Binding redirects moved from configuration to code

---
+++ PC.PowerApps/PC.PowerApps.FunctionApp/BindingRedirect.cs
@@ -0,0 +1,9 @@
+﻿namespace PC.PowerApps.FunctionApp
+{
+    public class BindingRedirect
+    {
+        public string ShortName { get; set; }
+        public string PublicKeyToken { get; set; }
+        public string RedirectToVersion { get; set; }
+    }
+}
