077c6c7388c8d660d7c25cdb74b6363b022a9d5d
Mihails Simvulidi <mihails.simvulidi@gmail.com>
2022-02-20 12:14:38 +0200

    Trigger Contact Required Participation Fee on Participation Fee Exemption create/update/delete

---
+++ PC.PowerApps/PC.PowerApps.Plugins/Bound/ParticipationFeeExemptions/PostCreateUpdate.cs
@@ -0,0 +1,27 @@
+﻿using PC.PowerApps.Common.Entities.Dataverse;
+using PC.PowerApps.Common.Repositories;
+using PC.PowerApps.Plugins.Contexts;
+using PC.PowerApps.Plugins.Enumerations;
+using System;
+
+namespace PC.PowerApps.Plugins.Bound.ParticipationFeeExemptions
+{
+    public class PostCreateUpdate : PluginBase
+    {
+        protected override void ExecuteInternal(IServiceProvider serviceProvider)
+        {
+            PostCreateUpdatePluginContext<pc_ParticipationFeeExemption> context = new(serviceProvider, User.System, User.User);
+            pc_ParticipationFeeExemption participationFeeExemption = context.PostImage;
+
+            if (context.GetIsAnyAttributeModified(p => new { p.pc_Contact, p.pc_From, p.pc_Till, p.StateCode }))
+            {
+                if (context.GetIsAnyAttributeModified(p => p.pc_Contact))
+                {
+                    ContactRepository.Recalculate(context, context.PreImage?.pc_Contact?.Id, participationLevel: false, requiredParticipationFee: true);
+                }
+
+                ContactRepository.Recalculate(context, participationFeeExemption.pc_Contact?.Id, participationLevel: false, requiredParticipationFee: true);
+            }
+        }
+    }
+}
---
+++ PC.PowerApps/PC.PowerApps.Plugins/Bound/ParticipationFeeExemptions/PostDelete.cs
@@ -0,0 +1,23 @@
+﻿using PC.PowerApps.Common.Entities.Dataverse;
+using PC.PowerApps.Common.Repositories;
+using PC.PowerApps.Plugins.Contexts;
+using PC.PowerApps.Plugins.Enumerations;
+using PC.PowerApps.Plugins.Extensions;
+using System;
+
+namespace PC.PowerApps.Plugins.Bound.ParticipationFeeExemptions
+{
+    public class PostDelete : PluginBase
+    {
+        protected override void ExecuteInternal(IServiceProvider serviceProvider)
+        {
+            DeletePluginContext<pc_ParticipationFeeExemption> context = new(serviceProvider, User.System, User.User);
+            pc_ParticipationFeeExemption participationFeeExemption = context.PreImage;
+
+            if (context.PluginExecutionContext.ParentContext?.IsDeleteOf(participationFeeExemption.pc_Contact) != true)
+            {
+                ContactRepository.Recalculate(context, participationFeeExemption.pc_Contact?.Id, participationLevel: false, requiredParticipationFee: true);
+            }
+        }
+    }
+}
